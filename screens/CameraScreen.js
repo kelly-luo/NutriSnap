2d892431887cdf6f8f3e2d600f8d082944afa924
kelly.luo <kelly.luo@datamars.com>
2021-10-03 20:46:01 +1300

    Uses camera expo for camera screen

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,18 +1,86 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+// import React from "react";
+// import { View, Text } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+// const CameraScreen = ({ navigation }) => {
+//   return (
+//     // <View>
+//     //   <SafeAreaView>
+//     //     <Button title="Close" onPress={() => navigation.goBack()} />
+//     //   </SafeAreaView>
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text>Camera!</Text>
+//     </View>
+//     // </View>
+//   );
+// };
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const CameraScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    // <View>
-    //   <SafeAreaView>
-    //     <Button title="Close" onPress={() => navigation.goBack()} />
-    //   </SafeAreaView>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Camera!</Text>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+            <MaterialCommunityIcons name="access-point" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
-    // </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+});
 
 export default CameraScreen;
