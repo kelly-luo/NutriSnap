e1361752c655c87cdec35b8251fc6e095f754541
kelly.luo <kelly.luo@datamars.com>
2021-10-03 20:25:34 +1300

    Configured app.json for expo build

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const CameraScreen = ({ navigation }) => {
   return (
+    // <View>
+    //   <SafeAreaView>
+    //     <Button title="Close" onPress={() => navigation.goBack()} />
+    //   </SafeAreaView>
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Camera!</Text>
     </View>
+    // </View>
   );
 };
 
