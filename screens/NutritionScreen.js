006a64c1c7556d9a513e6852f9fc9a90e17b6baf
kelly.luo <kelly.luo@datamars.com>
2021-10-17 14:26:37 +1300

    Template pie chart displays on nutri page

--- screens/NutritionScreen.js
+++ screens/NutritionScreen.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
+import PieChart from 'react-native-pie-chart'
+
 import { SIZES } from '../constants'
 
 const NutritionScreen = ({ route, navigation }) => {
@@ -13,19 +16,71 @@ const NutritionScreen = ({ route, navigation }) => {
     setFoodImage(foodImage)
   });
 
+  const widthAndHeight = 250
+  const series = [123, 321, 123, 789, 537]
+  const sliceColor = ['#F44336', '#2196F3', '#FFEB3B', '#4CAF50', '#FF9800']
+
   return (
-    <View style={{ height: SIZES.height * 0.35 }}>
-      <Image
-        source={foodImage}
-        resizeMode="cover"
-        style={{
-          width: SIZES.width,
-          height: "100%"
-        }}
-      />
-      {/* <Text style={{ fontSize: SIZES.h4, textAlign: 'center' }}>{timeConsumed}</Text> */}
-    </View>
+    <ScrollView>
+      <View style={{ height: SIZES.height * 0.35 }}>
+        <Image
+          source={foodImage}
+          resizeMode="cover"
+          style={{
+            width: SIZES.width,
+            height: "100%"
+          }}
+        />
+      </View>
+
+      {/* Pie Chart Information */}
+      <View>
+        <View style={styles.container}>
+          <Text style={styles.title}>Doughnut</Text>
+          <PieChart
+            widthAndHeight={widthAndHeight}
+            series={series}
+            sliceColor={sliceColor}
+            doughnut={true}
+            coverRadius={0.45}
+            coverFill={'#FFF'}
+          />
+        </View>
+
+        {/* <Text style={{ fontSize: SIZES.h4, textAlign: 'center' }}>{timeConsumed}</Text> */}
+
+        {/* Nutrition Information */}
+        <View style={styles.item}>
+          <Text> HIIII </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { alignItems: 'center', justifyContent: 'center', height: 500 },
+  gauge: {
+    position: 'absolute',
+    width: 100,
+    height: 160,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gaugeText: {
+    backgroundColor: 'transparent',
+    color: '#000',
+    fontSize: 24,
+  },
+  item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+})
 
 export default NutritionScreen;
