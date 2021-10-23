4a6f57211ec4443113d6960e51628cf4181bcce8
kelly.luo <kelly.luo@datamars.com>
2021-10-23 14:20:43 +1300

    Image uploads but is too big

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import { MediaLibrary } from 'expo-media-library';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useIsFocused } from '@react-navigation/native';
+import axios from 'axios';
+import mime from "mime";
 
 const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [imageUri, setImageUri] = useState(null);
   const isFocused = useIsFocused();
+
+  var FormData = require('form-data');
 
   useEffect(() => {
     (async () => {
@@ -17,7 +22,8 @@ const CameraScreen = ({ navigation }) => {
     })();
 
     const unsubscribe = navigation.addListener('focus', () => {
-      setImageUri(null);
+      // console.log(imageUri);
+      // setImageUri(null);
     });
 
     // Return the function to unsubscribe from the event so it gets removed on unmount
@@ -33,11 +39,70 @@ const CameraScreen = ({ navigation }) => {
 
   const takePicture = async () => {
     if (this.camera) {
-      const options = { quality: 1, base64: true };
+      const options = { quality: 1, base64: true, skipProcessing: true };
       const data = await this.camera.takePictureAsync(options);
       console.log(data.uri);
-      setImageUri(data.uri);
+      //const asset = await MediaLibrary.createAssetAsync(data.uri);
+      //setImageUri(asset);
+      //console.log(imageUri);
+      const formData = new FormData();
+      formData.append('image', {
+        uri: data.uri,
+        type: 'image/jpeg',
+        name: 'image.jpg',
+      });
+
+      var config = {
+        method: 'post',
+        url: 'https://api.logmeal.es/v2/image/recognition/complete/:model_version?skip_types=[1,3]&language=eng',
+        headers: {
+          'Authorization': 'Bearer c00399492c96866ead660e9acdedc33349383779',
+          'Content-Type': 'multipart/form-data',
+        },
+        data: formData
+      };
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      // postLogMealReq();
+
     }
+  };
+
+  const postLogMealReq = async () => {
+    const formData = new FormData();
+    console.log(imageUri);
+    const newImageUri = "file:///" + imageUri.split("file:/").join("");
+
+    formData.append('image', {
+      uri: newImageUri,
+      type: mime.getType(newImageUri),
+      name: newImageUri.split("/").pop(),
+    });
+
+    var config = {
+      method: 'post',
+      url: 'https://api.logmeal.es/v2/image/recognition/complete/:model_version?skip_types=[1,3]&language=eng',
+      headers: {
+        'Authorization': 'Bearer c00399492c96866ead660e9acdedc33349383779',
+        'Content-Type': 'multipart/form-data',
+        Accept: "application/json"
+      },
+      data: formData
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
